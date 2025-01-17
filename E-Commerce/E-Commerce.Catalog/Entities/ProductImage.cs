e41191b585e5475d5e7cf151a64d1826863dd222
Hamza Başaran <1basaranhamza@gmail.com>
2025-01-17 23:32:22 +0300

    MongoDb Connection

---
+++ E-Commerce/E-Commerce.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Commerce.Catalog.Entities
+{
+    public class ProductImage
+    {
+        public string ProductImageId { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
